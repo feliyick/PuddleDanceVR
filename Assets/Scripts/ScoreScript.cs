6b0abe044f59b0c6df92045682937a735c0f8ca3
Felicity Yick <59979404+feliyick@users.noreply.github.com>
2022-05-05 13:32:19 -0400

    more songs and basic streak

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+   
+    public int initialScore = 0;
+    public bool storeHighScore = true, allowNegative = true;
+    public Text field; // Text field displaying current score
+    public Text highScoreField; // Text field displaying high score
+    public Text streakField;
+
+    public int Score;
+    public int HighScore;
+    public int correctStreak;
+    public int scoreMultiplier;
+
+    void Awake() {
+        // Ensure only one instance is running
+        // if (instance == null)
+        //     instance = this; // Set instance to this object
+        // else
+        //     Destroy(gameObject); // Kill yo self
+        // Make sure the linked references didn't go missing
+        if (field == null)
+            Debug.LogError("Missing reference to 'field' on <b>SumScoreManager</b> component");
+        if (storeHighScore && highScoreField == null)
+            Debug.LogError("Missing reference to 'highScoreField' on <b>SumScoreManager</b> component");
+    }
+
+    void Start() {
+        Reset(); // Ensure score is 0 when object loads
+        if (initialScore != 0)
+            AddPoints(initialScore);  // Set initial score
+        if (storeHighScore) {
+            if (PlayerPrefs.HasKey("sumHS")) { 
+                // Set high score value and tell manager
+                HighScore = PlayerPrefs.GetInt("sumHS");
+                UpdatedHS();
+            }
+            else
+                HighScore = 0;
+        }
+
+        Updated(); // Set initial score in UI
+    }
+
+    /// <summary>Notify this manager of a change in score</summary>
+    void Updated () {
+        field.text = Score.ToString(); // Post new score to text field
+    }
+
+    /// <summary>Notify this manager of a change in high score</summary>
+    void UpdatedHS () {
+        if(storeHighScore)
+            highScoreField.text = HighScore.ToString(); // Post new high score to text field
+    }
+
+    public void AddPoints (int pointsToAdd) {
+        // Debug.Log(pointsToAdd + " points " + ((pointsToAdd > 0) ? "added" : "removed"));
+        Score += pointsToAdd;
+        if (correctStreak > 0) {
+            correctStreak += 1;
+        } else {
+            correctStreak = 0;
+        }
+        UpdateStreak();
+
+        if (Score < 0 && !allowNegative) {
+            Score = 0; // Reset score to 0
+            
+        }
+        Updated(); // Let the manager know we've changed the score
+    }
+
+    void UpdateStreak() {
+        if (correctStreak == 0) {
+            scoreMultiplier = 1;
+        } else if (correctStreak >= 10) {
+            scoreMultiplier = 2;
+        } else if (correctStreak >= 20) {
+            scoreMultiplier = 4;
+        } else if (correctStreak >= 40) {
+            scoreMultiplier = 8;
+        }
+        streakField.text = "x" + scoreMultiplier.ToString();
+    }
+
+    void Reset () {
+        Debug.Log("Reset score");
+        Score = 0;
+        Updated();
+
+    }
+}
+
+
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// /// <summary>Manager for SumScore accessible from inspector</summary>
+// /// <remarks>
+// /// Attach to game object in scene. 
+// /// This is a singleton so only one instance can be active at a time.
+// /// </remarks>
+// public class SumScoreManager : MonoBehaviour {
+
+    
+
+// }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+
 public class Spawner : MonoBehaviour
 {
 
     public GameObject[] targets;
     public Transform[] points;
+    public AudioClip[] audioClips;
     public float beat; // = (60/109)*2;
     private float timer;
     public GameObject player;
@@ -15,12 +18,56 @@ public class Spawner : MonoBehaviour
 
     //an AudioSource attached to this GameObject that will play the music.
     public AudioSource musicSource;
-
+    Dictionary<string, int> songIdxDict = new Dictionary<string, int>();
+    public Dictionary<string, Dictionary<int, float>> songBPMDict = new Dictionary<string, Dictionary<int, float>>();
     // Start is called before the first frame update
     void Start()
     {
         ProgressBar = GameObject.Find("ProgressBar").GetComponent<Slider>();
         Debug.Log("SLIDER START VALUE: " + ProgressBar.value);
+        initSongIdxDict();
+        initSongBPMDict();
+    }
+
+    void initSongIdxDict() {
+        songIdxDict.Add("EyeOfTheTiger", 0);
+        songIdxDict.Add("IWillSurvive", 1);
+        songIdxDict.Add("WhatDoesntKillYou", 2);
+    }
+
+    void initSongBPMDict() {
+        Dictionary<int, float> eyeOfTheTiger = new Dictionary<int, float>();
+        eyeOfTheTiger.Add(0, (60/109)*4);
+        eyeOfTheTiger.Add(1, (60/109)*2);
+        songBPMDict.Add("EyeOfTheTiger", eyeOfTheTiger);
+
+        Dictionary<int, float> iWillSurvive = new Dictionary<int, float>();
+        iWillSurvive.Add(0, (60/116)*4);
+        iWillSurvive.Add(1, (60/116)*2);
+        songBPMDict.Add("IWillSurvive", iWillSurvive);
+
+
+        Dictionary<int, float> stronger = new Dictionary<int, float>();
+        stronger.Add(0, (60/116)*4);
+        stronger.Add(1, (60/116)*2);
+        songBPMDict.Add("WhatDoesntKillYou", stronger);
+
+    }
+
+    void SetSong(string songChoice, int diffLevel) {
+        int idx = songIdxDict[songChoice];
+        musicSource.clip = audioClips[idx];
+        beat = songBPMDict["songChoice"][diffLevel];
+    }   
+
+    void StopSong() {
+        musicSource.Stop();
+    }
+
+    void ResetSpawner() {
+        timer = 0;
+        timerProgress = 0;
+        ProgressBar.value = 0;
     }
 
     // Update is called once per frame
